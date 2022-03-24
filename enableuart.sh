fb9eee489d8d976b6cd5aa66286c073fd8a3cd00
jhlee <jhlee@pharossvc.com>
2022-03-24 09:50:32 +0900

    INSTALL ADD] 인스톨 시 UART 활성화를 위한 스크립트 추가 (config.txt 에 uart enable 및 UART 포트 enable)

---
+++ enableuart.sh
@@ -0,0 +1,17 @@
+#!/bin/dash
+
+CONFIGFILE="/boot/config.txt"
+#CONFIGFILE="/workspace/src/radar/install/config.txt"
+RPI=`uname -a | grep raspberrypi | wc -l`
+
+if [ "0x" = "${RPI}x" ];then
+    echo "\nRun on RaspberryPI\nRun on RaspberryPI\n"
+    exit 0 
+fi
+
+EXIST=`cat ${CONFIGFILE} | grep "dtoverlay=uart0" | wc -l`
+if [ "0x" = "${EXIST}x" ];then
+    sudo sed -i 's/\[all\]/\[all\]\nenable_uart=1\ndtoverlay=uart0\ndtoverlay=uart3\ndtoverlay=uart4\ndtoverlay=uart5\n/' ${CONFIGFILE}
+else
+    echo "Already Setted\n"
+fi
--- install.sh
+++ install.sh
@@ -3,14 +3,17 @@
 RPI=`uname -a | grep raspberrypi | wc -l`
 
 if [ "0x" = "${RPI}x" ];then
-    echo "\nRun on RaspberryPI\nRun on RaspberryPI\n"
+    echo "\nRun on RaspberryPI\nRun on RaspberryPI CM4\n"
     exit 0 
 fi
 
 sudo apt install cmake
-source ./wiringpi.sh
-source ./setupupdate.sh
-
+echo "\nInstall Wiring Pi Library\n"
+. ./wiringpi.sh
+echo "\nMake default running Env\n"
+. ./setupupdate.sh
+echo "\nEnable UART\n"
+. ./enableuart.sh
 
 
 
