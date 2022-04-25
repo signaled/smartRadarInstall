8bb2c57e805cb21566ad5c0b089c2f1fcec9b90b
jhlee <jhlee@pharossvc.com>
2022-04-25 13:23:42 +0900

    Install ADD] signal 에 의한 종료 시 APP 재기동 실시

--- script/run.sh
+++ script/run.sh
@@ -3,14 +3,35 @@ APP=app
 RUNDIR=/home/pi
 UPDATEDIR=/home/pi/update
 BACKUPDIR=/home/pi/backup
+RET="255x"
+
 update()
 {
-	rm -rf ${BACKUPDIR}
-	mkdir -p ${BACKUPDIR}
-	mv ${RUNDIR}/${APP} ${BACKUPDIR}
-	mv ${UPDATEDIR}/${APP} ${RUNDIR}
-	chmod a+x ${RUNDIR}/${APP}
+    rm -rf ${BACKUPDIR}
+    mkdir -p ${BACKUPDIR}
+    mv ${RUNDIR}/${APP} ${BACKUPDIR}
+    mv ${UPDATEDIR}/${APP} ${RUNDIR}
+    chmod a+x ${RUNDIR}/${APP}
+}
+
+runApp()
+{
+    while [ "$RET" = "255x" ]
+    do
+        ${RUNDIR}/${APP} > /dev/null 2>&1
+        RET="$?x"
+        echo $RET
+        if [ "$RET" = "0x" ];then
+            echo "Exit w/ Normally\n"
+        else
+            RET="255x"
+            echo "Exception Occured\n"
+        fi
+    done
+    echo "fin"
+    
 }
 
 [ -f ${UPDATEDIR}/${APP} ] && update
-${RUNDIR}/${APP} > /dev/null 2>&1
+
+runApp
