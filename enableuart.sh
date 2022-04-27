c8b5697786feee5f7fb96b6f947423c907344cf0
jhlee <jhlee@pharossvc.com>
2022-04-27 14:28:52 +0900

    UART Modify] modify for rap

--- enableuart.sh
+++ enableuart.sh
@@ -1,7 +1,7 @@
 #!/bin/dash
 
-#CONFIGFILE="/boot/config.txt"
-CONFIGFILE="/workspace/src/smartRadarInstall/config.txt"
+CONFIGFILE="/boot/config.txt"
+#CONFIGFILE="/workspace/src/smartRadarInstall/config.txt"
 RPI=`uname -a | grep raspberrypi | wc -l`
 
 if [ "0x" = "${RPI}x" ];then
